0d59d2480cfbc0589f8dc223cc9d7480eeb02fe1
AdaZebra <rumusicmajor@yahoo.com>
2016-02-29 19:40:33 -0800

    BugFix
    Only problem was that the table was not connected to it’s code. Somehow
    it lost it’s connection.

---
+++ EatingRecorder/EatingRecorder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EatingRecorder
+//
+//  Created by GUEST on 2/29/16.
+//  Copyright © 2016 Edward Kuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EatingRecorder/EatingRecorder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EatingRecorder
+//
+//  Created by GUEST on 2/29/16.
+//  Copyright © 2016 Edward Kuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
